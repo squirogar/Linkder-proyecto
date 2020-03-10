3c5dc613e0b02159a5039b6110906655ee0224c9
squirogar <squirogar@ing.ucsc.cl>
2020-03-10 20:52:31 -0300

    login + registro. falta borrar actionbar

--- Linkder/app/src/main/java/com/example/linkder/LoginActivity.java
+++ Linkder/app/src/main/java/com/example/linkder/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.linkder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,14 +10,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView txtRegistrate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        txtRegistrate = findViewById(R.id.txtRegistrate);
+
+        txtRegistrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegistroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- Linkder/app/src/main/java/com/example/linkder/RegistroActivity.java
+++ Linkder/app/src/main/java/com/example/linkder/RegistroActivity.java
@@ -3,12 +3,23 @@ package com.example.linkder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RegistroActivity extends AppCompatActivity {
-
+    ImageView imgBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        imgBack = findViewById(R.id.imgBack);
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
